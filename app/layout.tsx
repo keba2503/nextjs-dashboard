a09deb04fd25ee4f4f69076c0cb623dc1f011a0f
Karen <karen.borrero@grupodinosol.es>
2024-05-05 17:51:52 +0100

    curso 🎉 begin a project

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './ui/global.css'
+import {montserrat} from "@/app/ui/fonts";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+      {children}
+      <footer className='py-10 flex justify-center items-center'>
+        Hecho con 💚 por Karen Borrero
+      </footer>
+      </body>
+    </html>
+  );
+}
