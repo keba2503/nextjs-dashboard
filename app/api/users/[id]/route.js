9a1e706ce0f044e9decbb11fa89d1f985c7e53fd
Karen <karen.borrero@grupodinosol.es>
2024-05-11 19:28:21 +0100

    users update

---
+++ app/api/users/[id]/route.js
@@ -0,0 +1,49 @@
+import {NextResponse} from "next/server";
+import bcrypt from "bcrypt";
+
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function PUT(request, { params }) {
+    try {
+        const data = await request.json();
+        console.log(data);
+
+        const id = parseInt(params.id);
+        const { name, lastname, email, password } = data;
+
+        const updateData = {};
+
+        if (name !== undefined && name !== null) {
+            updateData.name = name;
+        }
+        if (lastname !== undefined && lastname !== null) {
+            updateData.lastname = lastname;
+        }
+        if (email !== undefined && email !== null) {
+            updateData.email = email;
+        }
+        if (password !== undefined && password !== null && password !== '') {
+            updateData.password = await bcrypt.hash(password, 10);
+        }
+
+        const updateUser = await prisma.users.update({
+            where: { id },
+            data: updateData,
+        });
+
+        return NextResponse.json(updateUser);
+    } catch (error) {
+        return NextResponse.json(
+            {
+                message: error.message,
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+}
+
+
