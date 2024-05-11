d8f35855b82b47134566b7f9144179ef2c2960fa
Karen <karen.borrero@grupodinosol.es>
2024-05-11 19:00:45 +0100

    update api register

--- app/api/auth/singup/route.js
+++ app/api/auth/singup/route.js
@@ -1,7 +1,8 @@
-import { NextResponse } from "next/server";
+import {NextResponse} from "next/server";
 import bcrypt from "bcrypt";
 
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient} from '@prisma/client';
+
 const prisma = new PrismaClient();
 
 export async function POST(request) {
@@ -10,17 +11,35 @@ export async function POST(request) {
         const data = await request.json();
         console.log(data)
 
+        const userFound = await prisma.users.findFirst({
+            where: {
+                email: data.email,
+            },
+        });
+
+        console.log(userFound)
+        if (userFound) {
+            return NextResponse.json(
+                {
+                    message: "Email already exists",
+                },
+                {
+                    status: 400,
+                }
+            );
+        }
+
         const hashedPassword = await bcrypt.hash(data.password, 10);
         const newUser = await prisma.users.create({
             data: {
                 name: data.name,
-                lastname:data.lastname,
+                lastname: data.lastname,
                 email: data.email,
                 password: hashedPassword,
             },
         });
 
-        const { password: _, ...user } = newUser;
+        const {password: _, ...user} = newUser;
 
         return NextResponse.json(user);
     } catch (error) {
